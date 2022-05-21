7e3843bea195ab3d4ac624e642d7a379fc71888e
Dmitriy Tomilov <tomilov.d.a@mail.ru>
2022-05-21 14:41:03 +0400

    remove unused class

--- app/src/main/java/ru/dmitriyt/networkscanner/data/model/IsReachableResult.kt
+++
@@ -1,8 +0,0 @@
-package ru.dmitriyt.networkscanner.data.model
-
-sealed class IsReachableResult {
-    object NotExist : IsReachableResult()
-    class Exist(
-        val device: NetDevice,
-    ) : IsReachableResult()
-}
